834b4b2131bc10369f3d8451d06c541f4fcde3d5
XwyzWorm <pratamaazmi1@gmail.com>
2020-12-29 16:51:12 +0700

    Delete Listed Files and add FinalCode
    Udah di merge semuanya dan fix bug juga

--- bubblesort.py
+++
@@ -1,8 +0,0 @@
-arr = [0.22, 0.99, 0.88,0.21,0.11]
-def bubblesort(arr):
-    for i in range(0,len(arr)):
-        for j in range(0,len(arr) - i- 1):
-            arr[j] , arr[j+1] = arr[j+1] , arr[j]
-
-    print(arr)
-bubblesort(arr)
